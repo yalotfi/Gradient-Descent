930722db7b5f437a00f9fd9fec13a02ab89bc6c6
Yaseen Lotfi <yalotfi@outlook.com>
2017-01-21 19:41:14 -0500

    Implement gradient descent functions

--- linear_regression.py
+++ linear_regression.py
@@ -18,20 +18,45 @@ def compute_error(m, b, data):
     for i in range(0,len(points)):
     	x = points[i, 0] # x value at position i
     	y = points[i, 1] # y value at position i
-    	total_error += (y - (m * x + b)) ** 2 # Summation of squared values
+    	total_error += (y - (m * x + b)) ** 2 # Summation of squared distances
 
     # Return the average, total error of each point
     return total_error / float(len(points))
 
 
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+	# Starting y-intercept, b and slope, m
+	b = starting_b
+	m = starting_m
+
+	# Perform Gradient Descent
+	for i in range(num_iterations):
+		# Update b and m with more accurate version with each gradient step
+		b, m = step_gradient(b, m, array(points), learning_rate)
+	return [b, m]
+
+
 def step_gradient(b_current, m_current, points, learning_rate):
 	# Step Gradient function
-	return 0
+	b_gradient = 0
+	m_gradient = 0
+	n = float(len(points))
 
+	for i in range(0, len(points)):
+		x = points[i, 0]
+		y = points[i, 1]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
-	# Running gradient descent function
-	return 0
+		# Direction with respect to b and m
+		# Calculate partial derivative of each error at a given point
+		b_gradient += -(2/n) * (y - ((m_current * x) + b_current))
+		m_gradient += (2/n) * x * (y - ((m_current * x) + b_current))
+
+	# Update b and m values using the partial derivatives
+	new_b = b_current - (learning_rate * b_gradient)
+	new_m = m_current - (learning_rate * m_gradient)
+
+	# Return each b and m for every iteration
+	return [new_b, new_m] 
 
 
 def main():
